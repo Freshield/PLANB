aee0b0f8631cd985ffdcc0e3bd6fc84b02707799
Freshield <zxdsw199182@gmail.com>
2015-11-20 17:34:30 -0500

    PLANB init

---
+++ PLANB_EGO/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * PLANB_EGO.cpp
+ *
+ * Created: 2015/11/20 17:32:09
+ * Author : FRESHIELD
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
