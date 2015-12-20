857cc6058484aa2cb68b853eb05869ab344fb1b6
Freshield <zxdsw199182@gmail.com>
2015-12-19 21:30:25 -0500

    1602

---
+++ PLANB_LCD1602/PLANB_LCD1602/PLANB_LCD1602.cpp
@@ -0,0 +1,24 @@
+/*
+ * PLANB_LCD1602.cpp
+ *
+ * Created: 2015/10/6 19:48:18
+ *  Author: FRESHIELD
+ */ 
+
+#include <avr/io.h>
+#include <C:\Users\FRESHIELD\Documents\MEGAsync\EDEN\avr\system\lib_delay.h>
+#include <C:\Users\FRESHIELD\Documents\MEGAsync\EDEN\avr\device\lib_lcd1602_m16.h>
+
+int main(void)
+{
+	LCD_init(); 
+	
+	char line1[] = {0X2E,0X2E,0X2E,0XA7,0XC5,0XC0,0X2E,0X2E,0X2E,0x20,0x20,0x20,0x20,0x20,0x20,0x20};
+	char line2[] = {0XBD,0X2E,0X2E,0X2E,0XB7,0X2E,0X2E,0X2E,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20};
+	
+    while(1)
+    {
+        LCD_SAMPLE_SHOW_STRING(0,0,line1);
+		LCD_SAMPLE_SHOW_STRING(0,1,line2);
+    }
+}
